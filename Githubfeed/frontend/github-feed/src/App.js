431abdfb6c75cbd12518ab573fb56d91eb9feb74
pranit <patilpranit3112@gmail.com>
2022-09-30 15:42:10 +0530

    Initial Setup Completed

---
+++ Githubfeed/frontend/github-feed/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Userdetails from './components/Userdetails'
+
+function App() {
+  return (
+    <>
+        <Userdetails/>
+    </>
+    );
+}
+
+export default App;
---
+++ Githubfeed/frontend/github-feed/src/components/Userdetails.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+
+export default function UserDetails() {
+    
+return (
+    <div className="container">
+        <div className="userprofile">
+            <div className="image">
+                <img className="userimage" src="" alt="" />
+            </div>
+            <div className="userrdetails">
+                <div className="username">
+                    <p></p>
+                </div>
+                <div className="bio">
+                    <p></p>
+                </div>
+                <div className="location">
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
