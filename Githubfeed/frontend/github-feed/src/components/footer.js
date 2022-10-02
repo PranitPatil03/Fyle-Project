ba813e44666912421eb62eef3139d03491f57182
pranit <patilpranit3112@gmail.com>
2022-10-02 23:21:07 +0530

    Static Frontend Completed

--- Githubfeed/frontend/github-feed/src/components/footer.js
+++ Githubfeed/frontend/github-feed/src/components/footer.js
@@ -1,10 +1,11 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
 
 export default function footer() {
     return (
         <>
             <div className="box">
-                <div className="boxitems"><img src="https://github.com/PranitPatil03/Fyle-Project/blob/main/Githubfeed/frontend/github-feed/src/temp/images/rewind.png" alt="" /></div>
+                <div className="boxitems leftarrow"><img src="https://img.icons8.com/material-rounded/24/000000/rewind.png"/></div>
                 <div className="boxitem1 boxitems">1</div>
                 <div className="boxitem2 boxitems">2</div>
                 <div className="boxitem3 boxitems">3</div>
@@ -14,15 +15,15 @@ export default function footer() {
                 <div className="boxitem7 boxitems">7</div>
                 <div className="boxitem8 boxitems">8</div>
                 <div className="boxitem9 boxitems">9</div>
-                <div className="boxitems"><img src="https://github.com/PranitPatil03/Fyle-Project/blob/main/Githubfeed/frontend/github-feed/src/temp/images/forward.png" alt="" /></div>
+                <div className="boxitems rightarrow"><img src="https://img.icons8.com/material-rounded/24/000000/fast-forward.png"/></div>
             </div>
             <div className="buttons">
                 <div className="back">
-                    <img src="https://github.com/PranitPatil03/Fyle-Project/blob/main/Githubfeed/frontend/github-feed/src/temp/images/leftarrow.png" alt="" />
+                <img class="backimg" src="https://img.icons8.com/material-rounded/24/000000/long-arrow-left.png"/>
                     <p className="older">older</p>
                 </div>
                 <div className="forward">
-                    <img src="https://github.com/PranitPatil03/Fyle-Project/blob/main/Githubfeed/frontend/github-feed/src/temp/images/rightarrow.png" alt="" />
+                <img class="forwardimg" src="https://img.icons8.com/material-rounded/24/000000/long-arrow-right.png"/>
                     <p className="newer">newer</p>
                 </div>
             </div>
