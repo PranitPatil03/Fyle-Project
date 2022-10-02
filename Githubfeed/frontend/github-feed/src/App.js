e161b340751db5da409b946cd1e3dc1f11af670e
pranit <patilpranit3112@gmail.com>
2022-10-02 21:58:22 +0530

    Static Footer Component Completed

--- Githubfeed/frontend/github-feed/src/App.js
+++ Githubfeed/frontend/github-feed/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { RepoDetails } from './components/RepoDetails';
 import Userdetails from './components/Userdetails'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
         <Userdetails/>
         <RepoDetails/>
+        <Footer/>
     </>
     );
 }
---
+++ Githubfeed/frontend/github-feed/src/components/footer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function footer() {
+    return (
+        <>
+            <div className="box">
+                <div className="boxitems"><img src="https://github.com/PranitPatil03/Fyle-Project/blob/main/Githubfeed/frontend/github-feed/src/temp/images/rewind.png" alt="" /></div>
+                <div className="boxitem1 boxitems">1</div>
+                <div className="boxitem2 boxitems">2</div>
+                <div className="boxitem3 boxitems">3</div>
+                <div className="boxitem4 boxitems">4</div>
+                <div className="boxitem5 boxitems">5</div>
+                <div className="boxitem6 boxitems">6</div>
+                <div className="boxitem7 boxitems">7</div>
+                <div className="boxitem8 boxitems">8</div>
+                <div className="boxitem9 boxitems">9</div>
+                <div className="boxitems"><img src="https://github.com/PranitPatil03/Fyle-Project/blob/main/Githubfeed/frontend/github-feed/src/temp/images/forward.png" alt="" /></div>
+            </div>
+            <div className="buttons">
+                <div className="back">
+                    <img src="https://github.com/PranitPatil03/Fyle-Project/blob/main/Githubfeed/frontend/github-feed/src/temp/images/leftarrow.png" alt="" />
+                    <p className="older">older</p>
+                </div>
+                <div className="forward">
+                    <img src="https://github.com/PranitPatil03/Fyle-Project/blob/main/Githubfeed/frontend/github-feed/src/temp/images/rightarrow.png" alt="" />
+                    <p className="newer">newer</p>
+                </div>
+            </div>
+        </>
+    )
+}
